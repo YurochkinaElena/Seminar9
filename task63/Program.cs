1ce7d38dd1b548818ce72480c579225e2d3c5fa1
yurochkina.elena <transnab@mail.ru>
2022-12-12 18:09:37 +0300

    task63

---
+++ task63/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 63. Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке  от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+
+
+
+System.Console.WriteLine(PrintNumbers(1, 10));
+string PrintNumbers(int start, int end)
+{
+    if (start == end)
+    {
+        return start.ToString();
+    }
+    return (start + " " + PrintNumbers(++start, end));
+}
