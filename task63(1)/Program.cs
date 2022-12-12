507efee06186d20248abcef4aecd3521ff38eff4
yurochkina.elena <transnab@mail.ru>
2022-12-12 18:14:35 +0300

    task63(1)

---
+++ task63(1)/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача. Задайте значение M и N. Нпишиете программу, которая выведет все натуральные числа в промежутке от M до N.
+
+System.Console.WriteLine(PrintNumbers(100, 1000));
+string PrintNumbers(int start, int end)
+{
+    if (start == end)
+    {
+        return start.ToString();
+    }
+    return (start + " " + PrintNumbers(++start, end));
+}
